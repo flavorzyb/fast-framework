fb9bfdd51e54ea6797116033fe43643dc6ca951c
flavor <haker-haker@163.com>
2018-05-03 22:30:23 +0800

    add Container

---
+++ src/Contracts/Container/BindingResolutionException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Fast\Contracts\Container;
+
+use Exception;
+use Psr\Container\ContainerExceptionInterface;
+
+class BindingResolutionException extends Exception implements ContainerExceptionInterface
+{
+    //
+}
